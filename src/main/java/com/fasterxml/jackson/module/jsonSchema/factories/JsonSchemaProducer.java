b446e21f8fcb03b5e1d8693dbb3e5c44d80d035a
Tatu Saloranta <tsaloranta@gmail.com>
2012-11-22 20:42:55 -0800

    javadoc change

--- src/main/java/com/fasterxml/jackson/module/jsonSchema/factories/JsonSchemaProducer.java
+++ src/main/java/com/fasterxml/jackson/module/jsonSchema/factories/JsonSchemaProducer.java
@@ -3,9 +3,11 @@ package com.fasterxml.jackson.module.jsonSchema.factories;
 import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
 
 /**
+ * Interface for objects that produce {@link JsonSchema} instances;
+ * implemented by visitors.
+ * 
  * @author jphelan
  */
 public interface JsonSchemaProducer {
-
 	public JsonSchema getSchema();
 }
