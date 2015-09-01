f82823ecf4499bee0937f5e6273656bb6145e72e
Ignacio del Valle Alles <idelvall@brutusin.org>
2015-09-01 15:50:24 +0200

    https://github.com/FasterXML/jackson-module-jsonSchema/issues/27

--- src/test/java/com/fasterxml/jackson/module/jsonSchema/TestPropertyOrderInSchema.java
+++ src/test/java/com/fasterxml/jackson/module/jsonSchema/TestPropertyOrderInSchema.java
@@ -1,13 +1,46 @@
 package com.fasterxml.jackson.module.jsonSchema;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.MapperFeature;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class TestPropertyOrderInSchema extends SchemaTestBase {
 
-    @JsonPropertyOrder({"a", "b", "c"})
+    @JsonPropertyOrder({"c", "b", "a"})
     static class Bean {
+
+        private int b;
+        private String c;
+        private String a;
+
+        public int getB() {
+            return b;
+        }
+
+        public void setB(int b) {
+            this.b = b;
+        }
+
+        public String getC() {
+            return c;
+        }
+
+        public void setC(String c) {
+            this.c = c;
+        }
+
+        public String getA() {
+            return a;
+        }
+
+        public void setA(String a) {
+            this.a = a;
+        }
+    }
+    
+     @JsonPropertyOrder(alphabetic = true)
+    static class Bean2 {
 
         private int b;
         private String c;
@@ -43,11 +76,20 @@ public class TestPropertyOrderInSchema extends SchemaTestBase {
      /* Unit tests
      /**********************************************************
      */
-    final ObjectMapper MAPPER = objectMapper();
+    
 
-    public void testCorrectOrder() throws Exception {
+    public void testAnnotationOrder() throws Exception {
+        ObjectMapper MAPPER = objectMapper();
         JsonSchemaGenerator generator = new JsonSchemaGenerator(MAPPER);
         JsonSchema jsonSchema = generator.generateSchema(Bean.class);
+        System.out.println(jsonSchema.asObjectSchema().getProperties().keySet());
+        assertEquals(jsonSchema.asObjectSchema().getProperties().keySet().toString(), "[c, b, a]");
+    }
+    
+    public void testAlphabeticOrder() throws Exception {
+        final ObjectMapper MAPPER = objectMapper();
+        JsonSchemaGenerator generator = new JsonSchemaGenerator(MAPPER);
+        JsonSchema jsonSchema = generator.generateSchema(Bean2.class);
         System.out.println(jsonSchema.asObjectSchema().getProperties().keySet());
         assertEquals(jsonSchema.asObjectSchema().getProperties().keySet().toString(), "[a, b, c]");
     }
