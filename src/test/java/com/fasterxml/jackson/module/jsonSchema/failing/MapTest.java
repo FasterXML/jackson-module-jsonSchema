902a37ffd1ed3e0f79caa23b01a048a94f572792
Tatu Saloranta <tatu.saloranta@iki.fi>
2015-11-20 18:12:52 -0800

    Add a failing (with 2.6) test for #77

---
+++ src/test/java/com/fasterxml/jackson/module/jsonSchema/failing/MapTest.java
@@ -0,0 +1,50 @@
+package com.fasterxml.jackson.module.jsonSchema.failing;
+
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
+import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator;
+import com.fasterxml.jackson.module.jsonSchema.TestBase;
+
+public class MapTest extends TestBase
+{
+    static class MapBean {
+        private Map<String, Integer> counts;
+
+        public void setCounts(Map<String, Integer> counts) {
+            this.counts = counts;
+        }
+
+        public Map<String, Integer> getCounts() {
+            return counts;
+        }
+    }
+
+    /*
+    /**********************************************************
+    /* Tests methods
+    /**********************************************************
+    */
+
+    private final ObjectMapper MAPPER = new ObjectMapper();
+
+    public void testSimpleMaps() throws Exception
+    {
+        JsonSchemaGenerator generator = new JsonSchemaGenerator(MAPPER);
+        JsonSchema schema;
+
+        schema = generator.generateSchema(MapBean.class);
+        String json = MAPPER
+                .writerWithDefaultPrettyPrinter()
+                .writeValueAsString(schema);
+//        System.out.println(json);
+
+        // !!! TODO: actually verify key type is retained
+        fail("Key type not preserved, contents: "+json);
+        
+//        assertEquals("{\"type\":\"integer\"}", json);
+    }
+
+}
