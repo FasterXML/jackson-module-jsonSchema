711e68b1203c368b33442a3fd7720532e470f5d4
Tatu Saloranta <tatu.saloranta@iki.fi>
2016-09-27 22:52:19 -0700

    Revert #114 since it is not clear patch actually makes sense (should have read spec, asked question first before merging)

--- src/main/java/com/fasterxml/jackson/module/jsonSchema/types/StringSchema.java
+++ src/main/java/com/fasterxml/jackson/module/jsonSchema/types/StringSchema.java
@@ -9,90 +9,77 @@ import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
  * @author jphelan
  *
  */
-public class StringSchema extends ValueTypeSchema {
+public class StringSchema extends ValueTypeSchema
+{
+    /** this defines the maximum length of the string. */
+    @JsonProperty
+    private Integer maxLength;
 
-	/** this defines the maximum length of the string. */
-	@JsonProperty
-	private Integer maxLength;
+    /** this defines the minimum length of the string. */
+    @JsonProperty
+    private Integer minLength;
 
-	/** this defines the minimum length of the string. */
-	@JsonProperty
-	private Integer minLength;
-	/**
-	 * this provides a regular expression that a string instance MUST match in
-	 * order to be valid. Regular expressions SHOULD follow the regular
-	 * expression specification from ECMA 262/Perl 5
-	 */
-	@JsonProperty
-	private String pattern;
+    /**
+     * this provides a regular expression that a string instance MUST match in
+     * order to be valid. Regular expressions SHOULD follow the regular
+     * expression specification from ECMA 262/Perl 5
+     */
+    @JsonProperty
+    private String pattern;
 
-        /**
-         * This will be the media type
-         */
-        @JsonProperty
-        private String mediaType;
-        
-	@Override
-	public StringSchema asStringSchema() {
-		return this;
-	}
+    @Override
+    public StringSchema asStringSchema() {
+        return this;
+    }
 
-	public Integer getMaxLength() {
-	    return maxLength;
-	}
+    public Integer getMaxLength() {
+        return maxLength;
+    }
 
-	public Integer getMinLength() {
-	    return minLength;
-	}
+    public Integer getMinLength() {
+        return minLength;
+    }
 
-	public String getPattern() {
-	    return pattern;
-	}
+    public String getPattern() {
+        return pattern;
+    }
 
-        public String getMediaType() {
-            return mediaType;
-        }
-        
-	@Override
-	public JsonFormatTypes getType() {
-	    return JsonFormatTypes.STRING;
-	}
+    @Override
+    public JsonFormatTypes getType() {
+        return JsonFormatTypes.STRING;
+    }
 
-	@Override
-	public boolean isStringSchema() {
-	    return true;
-	}
+    @Override
+    public boolean isStringSchema() {
+        return true;
+    }
 
-	public void setMaxLength(Integer maxLength) {
-	    this.maxLength = maxLength;
-	}
+    public void setMaxLength(Integer maxLength) {
+        this.maxLength = maxLength;
+    }
 
-	public void setMinLength(Integer minLength) {
-	    this.minLength = minLength;
-	}
+    public void setMinLength(Integer minLength) {
+        this.minLength = minLength;
+    }
 
-	public void setPattern(String pattern) {
-	    this.pattern = pattern;
-	}
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
 
-        public void setMediaType(String mediaType) {
-            this.mediaType = mediaType;
-        }
-        
-     @Override
-     public boolean equals(Object obj)
-     {
-         if (obj == this) return true;
-         if (obj == null) return false;
-         if (!(obj instanceof StringSchema)) return false;
-         return _equals((StringSchema) obj);
-     }
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj == this) return true;
+        if (obj == null) return false;
+        if (!(obj instanceof StringSchema)) return false;
+        return _equals((StringSchema) obj);
+    }
 
-     protected boolean _equals(StringSchema that)
-     {
-         return equals(getMaxLength(), that.getMaxLength())
-                 && equals(getMinLength(), that.getMinLength())
-                 && equals(getPattern(), that.getPattern())
-                 && super.equals(that);
-     }
+    protected boolean _equals(StringSchema that)
+    {
+        return equals(getMaxLength(), that.getMaxLength())
+                && equals(getMinLength(), that.getMinLength())
+                && equals(getPattern(), that.getPattern())
+                && super.equals(that);
+    }
 }
