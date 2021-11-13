375133768788e4fd9b910927d1d7fdbf45fec4e6
Sun <ms19@fa21-cs527-067.cs.illinois.edu>
2021-11-13 13:25:04 -0600

    more fixes

--- src/test/java/com/fasterxml/jackson/module/jsonSchema/TestGenerateJsonSchema.java
+++ src/test/java/com/fasterxml/jackson/module/jsonSchema/TestGenerateJsonSchema.java
@@ -283,7 +283,6 @@ public class TestGenerateJsonSchema
 
         Map<String, Object> result = writeAndMap(mapper, jsonSchema);
         assertNotNull(result);
-        
         String schemaString = mapper.writeValueAsString(jsonSchema);
         assertEquals("{\"type\":\"object\"," +
                 "\"dependencies\":{\"property1\":[\"property2\"]}," +
