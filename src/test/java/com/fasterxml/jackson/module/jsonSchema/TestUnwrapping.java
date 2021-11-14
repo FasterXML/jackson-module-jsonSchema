3773d18dec8b3465b02821f08daae8d980f56409
Sun <ms19@fa21-cs527-067.cs.illinois.edu>
2021-11-14 01:26:40 -0600

    fix another test

--- src/test/java/com/fasterxml/jackson/module/jsonSchema/TestUnwrapping.java
+++ src/test/java/com/fasterxml/jackson/module/jsonSchema/TestUnwrapping.java
@@ -28,11 +28,12 @@ public class TestUnwrapping extends SchemaTestBase
 
     public void testUnwrapping()  throws Exception
     {
-        MAPPER.configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true);
-        JsonSchemaGenerator generator = new JsonSchemaGenerator(MAPPER);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true);
+        JsonSchemaGenerator generator = new JsonSchemaGenerator(mapper);
         JsonSchema schema = generator.generateSchema(UnwrappingRoot.class);
 
-        String json = MAPPER.writeValueAsString(schema).replace('"', '\'');
+        String json = mapper.writeValueAsString(schema).replace('"', '\'');
         
         String EXP = "{'type':'object'," +
                      "'id':'urn:jsonschema:com:fasterxml:jackson:module:jsonSchema:TestUnwrapping:UnwrappingRoot'," +
